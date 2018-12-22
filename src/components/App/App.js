e8876b1c77d3d6d6402e03537417b07e4c9f631a
n.satin <n.satin@skbkontur.ru>
2018-12-22 16:51:30 +0500

    merge games

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Lights from '../firstGame/Lights/Lights'
 import styles from './style.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Game from "../Game3/Game";
+import {Game2} from "../game2/Game2";
 export default class App extends React.Component {
     render () {
         return (
@@ -14,11 +15,19 @@ export default class App extends React.Component {
                             <Link to="/tree">ChristosTree</Link>
                         </li>
                         <li>
+                            <Link to="/lights">Lights</Link>
+                        </li>
+                        <li>
+                            <Link to="/game2">Game2</Link>
+                        </li>
+                        <li>
                             <Link to="/game3">Game3</Link>
                         </li>
                     </ul>
                     <hr />
                     <Route path="/tree" component={this.Tree} />
+                    <Route path="/lights" component={Lights} />
+                    <Route path="/game2" component={Game2} />
                     <Route path="/game3" component={Game} />
                 </div>
             </Router>
