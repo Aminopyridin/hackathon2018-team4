532cb26c15049329d8871d1ea1e51dcc3fc9d13b
n.satin <n.satin@skbkontur.ru>
2018-12-22 13:00:17 +0500

    mvp

---
+++ src/components/Game3/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return null;
+    }
+}
